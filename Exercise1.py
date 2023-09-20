3d5a9fc47505c993d2e5ec47bfac2a1bd10041b4
Zedvien Daniel Elias Vasquez Martinez <danielvas2005@gmail.com>
2023-09-20 11:41:49 -0500

    cambio Zedvien

--- Exercise1.py
+++ Exercise1.py
@@ -1,6 +1,6 @@
 """
 Fecha : 20/09/2023
-Autor: Nicolas Tapasco
+Autor: Nicolas Tapasco -- Zedvien Daniel Elias Vasquez Martinez
 Objetivo: Se requiere un software que calcule si un aprendiz tiene el estilo de aprendizaje Asimilador.
 Para ello deben realizarse 7 preguntas de respuesta SI o NO. Si el aprendiz obtuvo 4 o más respuestas
 en SI entonces el sistema deberá indicarle que es Asimilador, de lo contrario le dirá que su estilo de aprendizaje es otro."""
